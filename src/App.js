7e869a6b3e66c16c976cfe4f80e93c8c39ba01c6
Nicholas <nicholas@Nicholass-MacBook-Air.local>
2024-04-20 20:47:22 -0600

    updated files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
 import './App.css';
+import {getPrime} from './Prime.js';
+import React, { useEffect, useState } from 'react'; 
+import {Audio } from 'react-loader-spinner';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return(
+    <body className="App vertical-center">
+    { NumberLable()}
+    </body>
   );
 }
+
+function GenerateButton(func) {
+    return(
+        <button className = 'gen-button' onClick={func}>Generate</button>
+    );
+}
+
+
+function NumberLable() {
+    const [number, setNumber] = useState(0);
+    const [bits, setBits] = useState(512);
+    const [isLoading, setIsLoading] = useState(false);
+   //useEffect( () =>{setValue(isLoading ? 'loading' : number);}, [isLoading, number]);
+    useEffect( () =>{console.log(isLoading)}, [isLoading, number]);
+    
+    const getNumber =  () => {
+          setIsLoading(true);
+          const result = getPrime(bits);
+          setIsLoading(false);
+          setNumber(result);
+        //  setIsLoading(false);
+    }
+
+return(     
+        <div className='center-div'>
+            <h1 className='title'> Prime Number Generator </h1>
+            <div className='number-div' >
+
+            {isLoading ? loading() : number }
+            </div>
+            {BitsButtons(setBits)}
+            {GenerateButton(getNumber)}
+        </div>
+        );
+}
+
+function loading(){
+    return(
+    <Audio
+      height="80"
+      width="80"
+      radius="9"
+      color="green"
+      ariaLabel="loading"
+      wrapperStyle
+      wrapperClass
+        />);
+}
+
+function BitsButtons(setBits){
+    return(
+    <div 
+        onChange={() => 
+            setBits(document.querySelector('input[name="bits"]:checked').value)}
+        className = 'bit-selector'
+        >
+        <input type="radio" value='512' name="bits" /> 512
+        <input type="radio" value='1024' name="bits" /> 1024 
+        <input type="radio" value='2048' name="bits" /> 2048 
+    </div>
+    );
+}
+
 
 export default App;
---
+++ src/Prime.js
@@ -0,0 +1,72 @@
+/* global BigInt */
+
+export function getPrime(bits){
+    let test_number = getRandom(bits);
+    // Loop that breaks if rabin says a number is prime
+    while(! millarRabin(test_number)){
+        test_number = getRandom(bits);
+    }
+    return (test_number.toString());
+}
+
+// with the number of bits it returns a random odd number 
+function getRandom(bits){
+    let bitShift = BigInt(Math.round(Math.random() * 1));
+    bits = BigInt(bits);
+    let d = Math.floor(Math.random() * 200);
+    d = BigInt(d);
+    return((BigInt(2) ** (bits - bitShift)) * d - BigInt(1));
+}
+
+
+//  a ^ b mod n without blowing up the BigInt
+function modExp(a, b, n) {
+  a = a % n;
+  var result = 1n;
+  var x = a;
+  while(b > 0n){
+    var leastSignificantBit = b % 2n;
+    b = b / 2n;
+
+    if (leastSignificantBit === 1n) {
+      result = result * x;
+      result = result % n;
+    }
+
+    x = x * x;
+    x = x % n;
+  }
+  return result;
+}
+
+// Our prime number tester 
+function millarRabin(n){
+    n = BigInt(n);
+    let b = n - BigInt(1);
+    let i = BigInt(0);
+
+    while(b % 2n === BigInt(0)){
+        ++i;
+        b =  b / 2n;
+    }
+
+    for(let k = 0; k < 60; k++){
+        let a = Math.floor(Math.random() * (Number.MAX_SAFE_INTEGER - 2)) + 2;
+        a = BigInt(a)
+
+        let x = modExp(a, b, n) 
+
+        for(let s = 0; s < i; s++){
+            let y =  modExp(x, 2n, n);
+
+            if (y === 1n && x !== 1n && x !== n - 1n){
+                return(false);
+            }
+            if(y !== 1n){
+                return(false);
+         }
+            return(true);
+    } //for k
+}//func
+
+}
